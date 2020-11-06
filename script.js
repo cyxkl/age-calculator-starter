fcf0c68af284de11e08cf8aed7f0b724eb705ce2
Glitch (cyxkl-age-calculator-starter) <none>
2020-11-06 01:52:17 +0000

    🍪💃 Updated with Glitch

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+$("button").click(function(){
+  let name = $(".name").val();
+  let age = $(".year").val();
+  age = 2020 - age;
+  $(".message").append(`<p>Hello ${name}, you are ${age} years old.</p>`);
+  
+});
+console.log(name)
